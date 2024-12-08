fef5a287d01e79ca9bbddd304d911ad277c1f6d9
Shreshth Srivastav <44840043+ShreshthSrivastav@users.noreply.github.com>
2024-12-08 18:37:37 +0530

    Update app_bert_CB.py

--- app_bert_CB.py
+++ app_bert_CB.py
@@ -28,6 +28,7 @@ st.write("""Discover a smarter way to enhance seismic data integrity with our in
 Simply input an EBCDIC header, and our app will evaluate and improve the quality of your "Survey Name" metadata, providing an accuracy score for added confidence. Designed to optimize data management for seismic professionals, this app ensures your metadata meets the highest standards, paving the way for more reliable analysis and decision-making.
 
 While currently focused on "Survey Name" metadata, the app is built to scale, offering the potential to address a wide range of metadata attributes in the future. Whether you're streamlining existing data or preparing for expansive metadata corrections, our app is the perfect companion for quality-conscious geophysical workflows.
+
 Elevate your seismic metadata today—because precision starts here.""")
 
 st.subheader("Using prompt engineered LLM model:")
